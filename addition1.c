d66bb2fc481b2fd8fe6851dc263fb05c4249e2d9
Sarthak Singha <65425694+sarthaksingha@users.noreply.github.com>
2020-12-24 22:26:49 +0530

    Add files via upload

---
+++ addition1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+    int i;  // 'int' is a datatype of the variable, which is used to know the type of the variable. It tskes 16 bits or 2 bytes of memory. Other datatypes are char, float etc.
+    int j;
+    i=2;
+    j=3;
+    int k=i+j;
+
+    //printf("%d",k);
+    //printf("The result is %d",k);
+    printf("The addition result of %d & %d is %d",i,j,k);
+}
---
+++ addition2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+    int i;  // 'int' is a datatype of the variable, which is used to know the type of the variable. Other datatypes are char, float etc.
+    int j;
+    i=2;
+    j=3;
+    int k=i+j;
+
+    printf("The result is %d",k);
+}
