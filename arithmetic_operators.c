d5280759e2a3cfef618b3fd5cf7410a8ae53701a
Sarthak Singha <65425694+sarthaksingha@users.noreply.github.com>
2020-12-28 21:46:58 +0530

    Add files via upload

---
+++ arithmetic_operators.c
@@ -0,0 +1,23 @@
+// Arithmetic operators
+
+#include<stdio.h>
+
+int main()
+{
+    int i=9,j=3,k;
+
+    k=i+j;  // Addition '+' operator.
+    printf("The addition of %d and %d is %d\n",i,j,k);
+
+    k=i-j;  // Subtraction '-' operator.
+    printf("The subtraction of %d and %d is %d\n",i,j,k);
+
+    k=i*j;  // Multiplication '*' operator.
+    printf("The multiplication of %d and %d is %d\n",i,j,k);
+
+    k=i/j;  // Division '/' operator.
+    printf("The division of %d and %d is %d\n",i,j,k);
+
+    return 0;
+
+}
