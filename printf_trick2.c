c0f77e3eff659f4f9aeac0c4282964af6ade175c
Sarthak Singha <65425694+sarthaksingha@users.noreply.github.com>
2021-01-03 23:44:00 +0530

    Add files via upload

---
+++ printf_trick2.c
@@ -0,0 +1,12 @@
+/* A 'printf()' function trick. */
+/* Printing without semicolon (i.e. ';'). */
+
+
+#include<stdio.h>
+ int main()
+ {
+     if(printf("hello\n"))  // This 'printf()' method returns something. And 'if' needs that data.
+     {
+
+     }
+ }
