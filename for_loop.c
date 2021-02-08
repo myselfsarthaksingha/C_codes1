2bb0dfc28b289d184d06de26cced861c21adcd30
Sarthak Singha <65425694+sarthaksingha@users.noreply.github.com>
2021-02-08 22:06:58 +0530

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,17 @@
+// Write a program in C to print 'Hello' 5 times using for loop.
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    for(i=1;i<=5;i++)   // In case of for loop the initialization, condition, increment all are performed in a single line respectively.
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}
+
+
+/*
+Use of loop is execute some task multiple times.
+*/
