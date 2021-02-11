dd0e5c70ffd1823f57f8b7d3f23e8795d6efac22
Sarthak Singha <65425694+sarthaksingha@users.noreply.github.com>
2021-02-12 00:03:37 +0530

    Add files via upload

---
+++ pattern_printing3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=4;i++)
+    {
+        printf("*  ");
+    }
+    printf("\n");
+
+    for(i=1;i<=4;i++)
+    {
+        printf("*  ");
+    }
+    printf("\n");
+
+    for(i=1;i<=4;i++)
+    {
+        printf("*  ");
+    }
+    printf("\n");
+
+    for(i=1;i<=4;i++)
+    {
+        printf("*  ");
+    }
+    printf("\n");
+
+    return 0;
+}
